84f5faeb236f608cefeb1e142695270b05079ab3
gonz4216 <ethgon19@gmail.com>
2022-08-18 01:57:06 -0400

    main

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        
+            <nav id='nav--bar'>
+                <div className="row">
+                    <button onClick={}></button>
+                </div>
+            </nav>
+        
+    )
+}
+
+export default Nav
